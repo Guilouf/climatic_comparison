f6251cc9a4b9ff1d2fe862daa35bd0d8b8e1220f
guigui <guillaume.lebreton@inra.fr>
2018-11-28 11:31:58 +0100

    Export data to sqlite database, iterate diffrent locations

--- get_data.py
+++ get_data.py
@@ -1,13 +1,18 @@
-# https://power.larc.nasa.gov/docs/v1/
+"""
+https://power.larc.nasa.gov/docs/v1/
+"""
 
 import requests
 import csv
+from database import DataLine, ses
+from datetime import datetime
 
 
 class GetData:
         API_URL = 'https://power.larc.nasa.gov/cgi-bin/v1/DataAccess.py'
 
-        def __init__(self, latitude, longitude, start_date, end_date, param_list, elevation=None):
+        def __init__(self, location, latitude, longitude, start_date, end_date, param_list, elevation=None):
+            self.location = location
             self.latitude = latitude,
             self.longitude = longitude
             self.start_date = start_date
@@ -50,20 +55,42 @@ class GetData:
             return flipped
 
         def to_csv(self):
-            # print(self.shaped_data)
             with open('output.csv', 'w', encoding='utf-8') as csv_file:
                 writer = csv.writer(csv_file, delimiter=',', lineterminator='\n')
                 writer.writerow(['date', *self.param_real_list.keys()])  # order ok in 3.6
                 for time, value in self.shaped_data.items():  # maintenant les dicos sont ordonnés
                     writer.writerow([time, *value.values()])
-                    print([time, *value.values()])
+
+        def to_database(self):
+            object_list = []
+            for date, values in self.shaped_data.items():
+                try:
+                    obj = DataLine(date=datetime.strptime(date, '%Y%m%d'),
+                                   sky_insolation=values['ALLSKY_SFC_SW_DWN'],
+                                   precipitation=values['PRECTOT'],
+                                   temperature=values['T2M'],
+                                   temperature_max=values['T2M_MAX'],
+                                   temperature_min=values['T2M_MIN'],
+                                   location=self.location,
+                                   )
+                    object_list.append(obj)
+                except ValueError:  # une valeur manquante dans la ligne
+                    pass
+
+            # a ne surtout pas mettre ds la boucle, ca fait des integrity error et tu sais pas d'ou ca viens..
+            ses.bulk_save_objects(object_list)
+            ses.commit()
 
 
-msg = GetData(44.2490, 1.5659, 20160301, 20160331, ['T2M', 'PS', 'ALLSKY_SFC_SW_DWN'], elevation=274.62).to_csv()
+if __name__ == '__main__':
+    import json
 
+    param_list = ['T2M', 'T2M_MIN', 'T2M_MAX', 'PS', 'PRECTOT', 'ALLSKY_SFC_SW_DWN']
 
-# print('hoho', msg, msg.text, msg.url)
-# data = msg.json()['features'][0]['properties']['parameter']
-# print(data)
+    with open('locations.json', 'r') as locations_file:
+        locations = json.load(locations_file)
 
-# faut iterer par date tous les params, et virer les lignes incompletes..
+        for loc in locations:
+            GetData(loc['essai'], *loc['gps'], *loc['dates'],
+                    param_list,
+                    elevation=loc['elevation']).to_database()
