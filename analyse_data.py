f28e28376ca70914a32ab2f0efa35ea8f0d98465
guigui <guillaume.lebreton@inra.fr>
2018-11-28 14:30:34 +0100

    Forced pycharm to use default dialog, refactor analysis

--- analyse_data.py
+++ analyse_data.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 from database import DataLine, ses
+import matplotlib
 
-query_vendee = ses.query(DataLine).filter_by(location='vendee').filter(DataLine.date.between('2015-03-01', '2017-03-15'))
-query_charente = ses.query(DataLine).filter_by(location='charente').filter(DataLine.date.between('2015-03-01', '2017-03-15'))
+matplotlib.use('Qt5Agg')  # to force pycharm to not use its SciView
+
+query_vendee = ses.query(DataLine).filter_by(location='vendee').\
+    filter(DataLine.date.between('2015-03-01', '2018-01-15'))
+query_charente = ses.query(DataLine).filter_by(location='charente_maritime').\
+    filter(DataLine.date.between('2015-03-01', '2018-01-15'))
 
 
 fig, ax = plt.subplots()
@@ -11,9 +16,11 @@ fig, ax = plt.subplots()
 time = [q.date for q in query_vendee]
 temp = [q.temperature for q in query_vendee]
 sky = [q.sky_insolation for q in query_vendee]
+precip_vendee = [q.precipitation for q in query_vendee]
 
 ax.plot(time, temp, label='temp_vendée')
 ax.plot(time, sky, label='sky_vendée')
+ax.plot(time, precip_vendee, label='precip_vendée')
 
 time_charent = [q.date for q in query_charente]
 temp_charente = [q.temperature for q in query_charente]
