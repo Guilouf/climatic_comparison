f90f5a1bd832997f9fcc9352d33c38e7d568933e
Guilouf <Guilouf@users.noreply.github.com>
2018-12-03 15:12:32 +0100

    Created function to list column values, new filtering methods

--- analyse_data.py
+++ analyse_data.py
@@ -1,33 +1,24 @@
 import matplotlib.pyplot as plt
-from database import DataLine, ses
+from database import DataLine, ses, year, month, day, between
 import matplotlib
+
+
+def get_col(col_name, queryset):
+    return [getattr(obj, col_name) for obj in queryset]
+
 
 matplotlib.use('Qt5Agg')  # to force pycharm to not use its SciView
 
 query_vendee = ses.query(DataLine).filter_by(location='vendee').\
-    filter(DataLine.date.between('2015-03-01', '2018-01-15'))
-query_charente = ses.query(DataLine).filter_by(location='charente_maritime').\
-    filter(DataLine.date.between('2015-03-01', '2018-01-15'))
+    filter(DataLine.date.between('2018-02-01', '2018-02-15'))
 
+query_charente = ses.query(DataLine).filter_by(location='charente_maritime').filter(month > 2, month < 5)
 
 fig, ax = plt.subplots()
 
 
-time = [q.date for q in query_vendee]
-temp = [q.temperature for q in query_vendee]
-sky = [q.sky_insolation for q in query_vendee]
-precip_vendee = [q.precipitation for q in query_vendee]
-
-ax.plot(time, temp, label='temp_vendée')
-ax.plot(time, sky, label='sky_vendée')
-ax.plot(time, precip_vendee, label='precip_vendée')
-
-time_charent = [q.date for q in query_charente]
-temp_charente = [q.temperature for q in query_charente]
-sky_charente = [q.sky_insolation for q in query_charente]
-
-ax.plot(time_charent, temp_charente, label='temp_charente')
+ax.plot(get_col('date_month_day', query_charente.filter_by(date_year=2018)),
+        get_col('temperature', query_charente.filter_by(date_year=2018)), label='temp_charente_2018')
 
 ax.legend()
-
 plt.show()
