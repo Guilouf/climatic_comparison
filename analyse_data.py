b286dc18cf16310ee137dcf13db97865f26dda07
Guilouf <Guilouf@users.noreply.github.com>
2020-04-14 19:30:32 +0200

    Extract demo

--- analyse_data.py
+++ analyse_data.py
@@ -12,14 +12,17 @@ matplotlib.use('Qt5Agg')  # to force pycharm to not use its SciView
 query_vendee = ses.query(DataLine).filter_by(location='vendee').\
     filter(DataLine.date.between('2018-02-01', '2018-02-15'))
 
-query_charente = ses.query(DataLine).filter_by(location='charente_maritime').filter(month > 2, month < 5)
+# query_charente = ses.query(DataLine).filter_by(location='charente_maritime').filter(month > 2, month < 5)
+query_charente = ses.query(DataLine).filter_by(location='charente_maritime', date_month_day=(3, 25))
+
+print(get_col('temperature', query_charente))
 
 fig, ax = plt.subplots()
 fig.autofmt_xdate()
 
 
-ax.plot(get_col('date', query_charente.filter_by(date_year=2018)),
-        get_col('temperature', query_charente.filter_by(date_year=2018)), label='temp_charente_2018')
+ax.plot(get_col('date', query_charente),
+        get_col('temperature', query_charente), label='temp_charente_2018')
 
 ax.legend()
 plt.show()
